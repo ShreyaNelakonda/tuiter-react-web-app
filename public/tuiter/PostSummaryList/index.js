c3a171e306cbc4922e9815b33ebcd3cc7ab3f68e
Shreya Nelakonda <shreyanelakonda@shreyas-mbp.mynetworksettings.com>
2022-10-15 23:01:58 -0400

    a5 JavaScript fall22

---
+++ public/tuiter/PostSummaryList/index.js
@@ -0,0 +1,13 @@
+import PostSummaryItem from "./PostSummaryItem.js";
+import posts from "./posts.js"
+const PostSummaryList = () => {
+    return (`
+            ${
+                posts.map(posts => {
+                    return(PostSummaryItem(posts));
+                }).join('')
+            }
+    `);
+}
+
+export default PostSummaryList;
