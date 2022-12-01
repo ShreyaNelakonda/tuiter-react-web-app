570099cbdbc9fefc8514285eae4f003df5803d41
Shreya Nelakonda <shreyanelakonda@Shreyas-MacBook-Pro.local>
2022-11-30 21:45:07 -0500

    a9 Mongo fa22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const TUITS_API = 'https://tuiter-node-server-app.herokuapp.com/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-app9.herokuapp.com/api/tuits';
+
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
